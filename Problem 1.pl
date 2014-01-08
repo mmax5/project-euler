efbd13051f56d273950f7c3301163e60ddbe3ea4
michael max <mmax@envy>
2014-01-08 18:27:40 -0500

    Commited wrong problem for problem 1.

--- Problem 1.pl
+++ Problem 1.pl
@@ -2,17 +2,13 @@
 
 use strict;
 use warnings;
-use Math::Complex;
 
-$sum = 0;
-$last = 1;
+$sum = 0
 
-for($i = 1; $i < 4000000;){
-	$i = $i + $last;
-	$last = $i - $last;
-	if($i % 2 == 0){
-		$sum += $i;
-		}
+for($i = 0; $i < 100; $i++){
+	if ($i % 5 == 0 || $i %3 == 0)
+		$sum += $i
+	
 	}
 	
-print "$sum";
+	print "$sum";
