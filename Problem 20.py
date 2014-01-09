1be9694921a324683f533cef683847297663199c
michael max <mmax@envy>
2014-01-08 19:01:38 -0500

    Cleaned up timing code

--- Problem 20.py
+++ Problem 20.py
@@ -1,31 +1,4 @@
-__author__ = 'mmax'
-from time import time
 from math import factorial
-s = time()
 print sum(int(y) for y in str(factorial(100)))
 
-print 'TIME : ',time() - s,' seconds'
 
-s = time()
-def factorial(n):
-    a=2
-    fact=1
-    while a<=n:
-        fact=fact*a
-        a=a+1
-
-    return fact
-
-def sum_digits(n):
-    string=str(n)
-    a=0
-    sum=0
-    while a<len(string):
-        sum=sum+int(string[a])
-        a=a+1
-    return sum
-
-x=sum_digits(factorial(100))
-
-print x
-print 'TIME : ',time() - s,' seconds'
